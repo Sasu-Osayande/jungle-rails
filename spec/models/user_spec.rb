1052c46084e5d1c0044fdbf134dab9d4274b39fb
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-12 12:32:00 -0400

    Define the validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "is valid when password and password confirmation match" do
+    end
+    it "is invalid when password and password confirmation do not match" do
+    end
+    it "is invalid when password is empty" do
+    end
+    it "is invalid when password confirmation is empty" do
+    end
+    it "is invalid when email is not unique" do
+    end
+    it "is invalid when email is empty" do
+    end
+    it "is invalid when first name is empty" do
+    end
+    it "is invalid when last name is empty" do
+    end
 end
